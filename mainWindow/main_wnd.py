4984bf5d9f70ae258a8a85ab7d531484dc0cb8d0
zhilin yura <zhilin_yura001@vk.com>
2024-10-26 11:30:27 +0500

    not need

--- mainWindow/main_wnd.py
+++ mainWindow/main_wnd.py
@@ -1,25 +1,34 @@
-import sys
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QMessageBox
 from PyQt6.QtCore import Qt
-class MainWindow(QMainWindow):
+
+
+class MainUI():
     def __init__(self):
         super().__init__()
+        self.download_btn = QPushButton("Загрузить изображение")
+        self.show_hst_btn = QPushButton("Показать историю результатов")
+        self.setting_btn = QPushButton("Настройки приложения")
+        self.layout_main = QVBoxLayout()
 
+        
+    def setup_main_ui(self):
         self.resize(600, 600)
         self.setWindowTitle("Главная страница")
+        title = QLabel("здарова это приложение команды восход загружаете")
+        self.download_btn.setFixedSize(300, 30)
+        self.setting_btn.setFixedSize(300, 30)
+        self.show_hst_btn.setFixedSize(300, 30)
 
-        download_btn = QPushButton("Загрузить изображение")
-        show_hst_btn = QPushButton("Показать историю результатов")
-        download_btn.setFixedSize(300, 30)
-        show_hst_btn.setFixedSize(300, 30)
-
-        layout = QVBoxLayout()
-        layout.addWidget(download_btn, alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(show_hst_btn, alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.addStretch(10)
-
+        self.layout_main.addWidget(self.download_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.layout_main.addWidget(self.show_hst_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.layout_main.addWidget(self.setting_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.layout_main.addSpacing(40)
+        self.layout_main.addWidget(title, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.layout_main.addStretch()
+  
         central_widget = QWidget()
-        central_widget.setLayout(layout)
-
+        central_widget.setLayout(self.layout_main)
         self.setCentralWidget(central_widget)
+
+
 
